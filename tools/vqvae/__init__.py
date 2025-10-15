c884a895f7716fde45acfe1646e66a735236f975
Simon Luder <simon.luder@students.fhnw.ch>
2025-10-15 20:59:51 +0200

    Update __init__.py in vqvae

--- tools/vqvae/__init__.py
+++ tools/vqvae/__init__.py
@@ -1,3 +1,6 @@
-from . import train, validate, test, inference
+from .train import train
+from .validate import validate
+from .test import test
+from .inference import inference
 
 __all__ = ["train", "validate", "test", "inference"]
